215e6b11ccc5072cdd7ce2489022c8c89657b9ad
phcoronatracker <62355247+phcoronatracker@users.noreply.github.com>
2020-03-19 03:44:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Philippines COVID-19 Tracker");
+
+update('cases', 202);
+update('death', 17);
+update('recover', 7);
+update('pui', 338);
+update('pum', 2825);
+
+function update(id, count) {
+    document.getElementById(id).innerHTML = (`${count}`);
+    console.log(`${id} = ${count}`);
+}
+
+function search_ph() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("input_ph");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ph");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
+
+function search_ncr() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("input_ncr");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ncr");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
