c57bbfb6ca38c6a19af1746306868aadbd61c0da
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-03-26 02:10:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,41 @@ function search_ncr() {
         }       
     }
 }
+
+function search_luzon() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("input_luzon");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("luzon");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
+
+function search_vimin() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("input_vimin");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("vimin");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
