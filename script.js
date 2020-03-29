ab1c73da243cbfa381d840281cffad26d15e83a9
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-03-30 03:43:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("Philippines COVID-19 Tracker");
 
-update('cases', 552);
-update('death', 35);
-update('recover', 20);
-update('pui', 615);
+update('cases', 1418);
+update('death', 71);
+update('recover', 42);
+update('pui', 874);
 update('pum', 6321);
 
 function update(id, count) {
