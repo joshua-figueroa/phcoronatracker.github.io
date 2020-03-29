ba8731bbc5109e47098e2712f859846954d585b1
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-03-30 03:44:12 +0800

    Update chart.js

--- chart.js
+++ chart.js
@@ -20,14 +20,16 @@ let chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['March 1', 'March 8', 'March 15', 'March 22', 'March 26', 'March 27', 'March 28'],
+        labels: ['March 1', 'March 8', 'March 15', 'March 22', 'March 27', 'March 28', 'March 29'],
         datasets: [{
             label: 'Number of Cases',
             backgroundColor: 'rgba(255, 255, 255, 0)',
             borderColor: 'rgb(255, 255, 255)',
             pointBackgroundColor: 'rgb(255, 255, 255)',
             pointRadius: 4,
-            data: [3, 10, 140, 462, 707, 803, 1075]
+            hitRadius: 1,
+            hoverRadius: 5,
+            data: [3, 10, 140, 462, 803, 1075, 1418]
         }]
     },
 
