1cdd170d5c91a72e0506b998f07a909a244cef15
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-19 23:55:47 +0800

    Update map.js

--- map.js
+++ map.js
@@ -1,234 +1,3 @@
-let citymap = {
-    manila: {
-        center: {lat: 14.599512, lng: 120.984222},
-        cases: 3785
-    }, 
-    rizal: {
-        center: {lat: 14.6037, lng: 121.3084},
-        cases: 300
-    },
-    bulacan: {
-        center: {lat: 14.8527, lng: 120.8160},
-        cases: 99
-    },
-    cavite: {
-        center: {lat: 14.4791, lng: 120.8970},
-        cases: 188
-    },
-    laguna: {
-        center: {lat: 14.2691, lng: 121.4113},
-        cases: 219
-    },
-    batangas: {
-        center: {lat: 13.7565, lng: 121.0583},
-        cases: 76
-    },
-    cagayan: {
-        center: {lat: 17.6132, lng: 121.7270},
-        cases: 17
-    },  
-    davao_sur: {
-        center: {lat: 7.1907, lng: 125.4553},
-        cases: 80
-    }, 
-    iloilo: {
-        center: {lat: 10.7202, lng: 122.5621},
-        cases: 22
-    }, 
-    isabela: {
-        center: {lat: 16.9754, lng: 121.8107},
-        cases: 11
-    }, 
-    lanao_sur: {
-        center: {lat: 7.8232, lng: 124.4198},
-        cases: 8
-    }, 
-    negros_occ: {
-        center: {lat: 10.2926, lng: 123.0247},
-        cases: 12
-    }, 
-    pampanga: {
-        center: {lat: 15.0794, lng: 120.6200},
-        cases: 57
-    }, 
-    benguet: {
-        center: {lat: 16.4023, lng: 120.5960},
-        cases: 20
-    }, 
-    bataan: {
-        center: {lat: 14.6417, lng: 120.4818},
-        cases: 36
-    }, 
-    la_union: {
-        center: {lat: 16.6159, lng: 120.3209},
-        cases: 14
-    }, 
-    negros_or: {
-        center: {lat: 9.6282, lng: 122.9888},
-        cases: 7
-    }, 
-    nueva_ecija: {
-        center: {lat: 15.5784, lng: 121.1113},
-        cases: 47
-    }, 
-    abra: {
-        center: {lat: 17.5951, lng: 120.7983},
-        cases: 3
-    }, 
-    aklan: {
-        center: {lat: 11.8166, lng: 122.0942},
-        cases: 7
-    }, 
-    capiz: {
-        center: {lat: 11.3889, lng: 122.6277},
-        cases: 4
-    }, 
-    camsur: {
-        center: {lat: 13.5250, lng: 123.3486},
-        cases: 10
-    }, 
-    cebu: {
-        center: {lat: 10.3157, lng: 123.8854},
-        cases: 42
-    }, 
-    cotabato_city: {
-        center: {lat: 7.2047, lng: 124.2310},
-        cases: 6
-    }, 
-    davao_oro: {
-        center: {lat: 7.5125, lng: 126.1763},
-        cases: 2
-    }, 
-    lanao_norte: {
-        center: {lat: 7.8722, lng: 123.8858},
-        cases: 5
-    }, 
-    oriental_mindoro: {
-        center: {lat: 13.0565, lng: 121.4069},
-        cases: 6
-    }, 
-    sulatan_kudarat: {
-        center: {lat: 6.5069, lng: 124.4198},
-        cases: 3
-    },
-    tarlac: {
-        center: {lat: 15.4755, lng: 120.5963},
-        cases: 25
-    },
-    quezon: {
-        center: {lat: 13.9414, lng: 121.6234},
-        cases: 34
-    },
-    zamboanga_sur: {
-        center: {lat: 7.8383, lng: 123.2967},
-        cases: 11
-    },
-    davao_norte: {
-        center: {lat: 7.5618, lng: 125.6533},
-        cases: 12
-    },
-    nueva_viscaya: {
-        center: {lat: 16.3301, lng: 121.1710},
-        cases: 5
-    },
-    davao_oriental: {
-        center: {lat: 7.3172, lng: 126.5420},
-        cases: 3
-    },
-    pangasinan: {
-        center: {lat: 15.8949, lng: 120.2863},
-        cases: 36
-    },
-    zambales: {
-        center: {lat: 15.5082, lng: 119.9698},
-        cases: 21
-    },
-    south_cotabato: {
-        center: {lat: 6.3358, lng: 124.7741},
-        cases: 4
-    },
-    albay: {
-        center: {lat: 13.1775, lng: 123.5280},
-        cases: 15
-    },
-    samar: {
-        center: {lat: 11.5795, lng: 124.9748},
-        cases: 4
-    },
-    misamis_occ: {
-        center: {lat: 8.3375, lng: 123.7071},
-        cases: 3
-    },
-    ilocos_sur: {
-        center: {lat: 17.2279, lng: 120.5740},
-        cases: 3
-    },
-    romblon: {
-        center: {lat: 12.5778, lng: 122.2691},
-        cases: 1
-    },
-    catanduanes: {
-        center: {lat: 13.7089, lng: 124.2422},
-        cases: 1
-    },
-    antique: {
-        center: {lat: 11.3806, lng: 122.0635},
-        cases: 1
-    },
-    camiguin: {
-        center: {lat: 9.1732, lng: 124.7299},
-        cases: 1
-    },
-    bukidnon: {
-        center: {lat: 8.0515, lng: 124.9230},
-        cases: 1
-    },
-    agusan_norte: {
-        center: {lat: 8.9456, lng: 125.5319},
-        cases: 1
-    },
-    maguindanao: {
-        center: {lat: 6.9423, lng: 124.4198},
-        cases: 1
-    },
-    cotabato: {
-        center: {lat: 7.204667, lng: 124.231044},
-        cases: 4
-    },
-    ilocos_norte: {
-        center: {lat: 18.1647, lng: 120.7116},
-        cases: 6
-    },
-    quirino: {
-        center: {lat: 16.2700, lng: 121.5370},
-        cases: 2
-    },
-    marinduque: {
-        center: {lat: 13.4767, lng: 121.9032},
-        cases: 4
-    },
-    palawan: {
-        center: {lat: 9.7638, lng: 118.7473},
-        cases: 2
-    },
-    masbate: {
-        center: {lat: 12.3574, lng: 123.5504},
-        cases: 1
-    },
-    bohol: {
-        center: {lat: 9.83333, lng: 124.166666},
-        cases: 2
-    },
-    northern_samar: {
-        center: {lat: 12.4198, lng: 124.6414},
-        cases: 1
-    },
-    misamis_or: {
-        center: {lat: 8.5046, lng: 124.6220},
-        cases: 2
-    }
-};
-
 function initMap() {
     // Styles a map in night mode.
     var location = {lat: 12.879721, lng: 121.774017};
@@ -316,18 +85,36 @@ function initMap() {
             }
         ]
     });
-
-    for(var city in citymap) {
-        // Add the circle for this city to the map.
-        var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: citymap[city].center,
-            radius: Math.sqrt(citymap[city].cases) * 2000
-        });
-    }
+    $.ajax({
+        url: 'https://phcoronatracker.com/cases.json',
+        dataType: 'json',
+        success: function(data) {
+            for(var i = 1; i < data.length-1; i++) {
+                var cityCircle = new google.maps.Circle({
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35,
+                    map: map,
+                    center: data[i].center,
+                    radius: Math.sqrt(data[i].cases) * 2000,
+                    content: '<h6>' + data[i].name + '</h6><span>Cases - ' + data[i].cases + '</span><br><span>Deaths - ' + data[i].death + '</span><br><span>Recoveries - ' + data[i].rec + '</span>'
+                });
+                
+                var infoWindow = new google.maps.InfoWindow();
+                google.maps.event.addListener(cityCircle, 'mouseover', function(e) {
+                    infoWindow.setContent(this.content);
+                    infoWindow.setPosition(this.getCenter());
+                    infoWindow.open(map);
+                });
+                google.maps.event.addDomListener(cityCircle, 'mouseout', function(e) {
+                    infoWindow.close();
+                });
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('Error: ' + textStatus + ' - ' + errorThrown);
+        }
+    });
 }
