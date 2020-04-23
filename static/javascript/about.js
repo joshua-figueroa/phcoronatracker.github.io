7b7cf6bbab06cd307c9e609c49e89d7fc00fb855
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-24 00:57:00 +0800

    Create about.js

---
+++ static/javascript/about.js
@@ -0,0 +1,14 @@
+$('#top').hide();
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() > 100) {
+            $('#top').fadeIn(75);
+        }
+        else {
+            $('#top').fadeOut();
+        }
+    });
+    $('#top').on('click', function() {
+        $('html, body').animate({scrollTop: 0}, "fast");
+    });
+});
