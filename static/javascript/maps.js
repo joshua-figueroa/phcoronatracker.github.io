d86a933aa7bdb307adf7d987cf1ad1e1fe5e4b1a
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-24 00:58:28 +0800

    Create maps.js

---
+++ static/javascript/maps.js
@@ -0,0 +1,121 @@
+function initMap() {
+    // Styles a map in night mode.
+    var location = {lat: 12.879721, lng: 121.774017};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: location,
+        zoom: 6,
+        mapTypeId: 'terrain',
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+            },
+            {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+            },
+            {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#263c3f'}]
+            },
+            {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#6b9a76'}]
+            },
+            {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#38414e'}]
+            },
+            {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#212a37'}]
+            },
+            {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9ca5b3'}]
+            },
+            {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#746855'}]
+            },
+            {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+            },
+            {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#f3d19c'}]
+            },
+            {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#2f3948'}]
+            },
+            {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+            },
+            {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#17263c'}]
+            },
+            {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#515c6d'}]
+            },
+            {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#17263c'}]
+            }
+        ]
+    });
+    $.ajax({
+        url: 'https://phcoronatracker.com/static/JSON/cases.json',
+        dataType: 'json',
+        success: function(data) {
+            for(var i = 1; i < data.length-1; i++) {
+                var cityCircle = new google.maps.Circle({
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35,
+                    map: map,
+                    center: data[i].center,
+                    radius: Math.sqrt(data[i].cases) * 2000,
+                    content: '<h6>' + data[i].name + '</h6><span>Cases - ' + data[i].cases + '</span><br><span>Deaths - ' + data[i].death + '</span><br><span>Recoveries - ' + data[i].rec + '</span>'
+                });
+                
+                var infoWindow = new google.maps.InfoWindow();
+                google.maps.event.addListener(cityCircle, 'mouseover', function(e) {
+                    infoWindow.setContent(this.content);
+                    infoWindow.setPosition(this.getCenter());
+                    infoWindow.open(map);
+                });
+                google.maps.event.addDomListener(cityCircle, 'mouseout', function(e) {
+                    infoWindow.close();
+                });
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('Error: ' + textStatus + ' - ' + errorThrown);
+        }
+    });
+}
