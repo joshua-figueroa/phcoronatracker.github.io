d77b6ae303eb3f837aef7986825ef2b47b2eed0f
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-24 00:57:46 +0800

    Create script.js

---
+++ static/javascript/script.js
@@ -0,0 +1,61 @@
+$('#top').hide();       
+$('#myModal').modal('show');
+$(document).ready(function() {  
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() > 100) {
+            $('#top').fadeIn(75);
+        }
+        else {
+            $('#top').fadeOut();
+        }
+    });
+    $('#top').on('click', function() {
+        $('html, body').animate({scrollTop: 0}, "fast");
+    });
+    update('cases', 6981);
+    update('deaths', 462);
+    update('recover', 722);
+    update('validation', 205);
+    function table(link, id) {
+        $.ajax({
+            url: link,
+            dataType: 'json',
+            success: function(data) {
+                for(var i = 0; i < data.length; i++) {
+                    var row = $('<tr><th scope="row">' + data[i].name + '</th><td>' + data[i].cases + '</td><td>' + data[i].death +'</td><td>' + data[i].rec + '</td></tr>');
+                    $(id).append(row);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert('Error: ' + textStatus + ' - ' + errorThrown);
+            }
+        });
+    }
+    table('https://phcoronatracker.com/static/JSON/cases.json', '#ph_body');
+    table('https://phcoronatracker.com/static/JSON/ncr.json', '#ncr_body');
+    table('https://phcoronatracker.com/static/JSON/luzon.json', '#luzon_body');
+    table('https://phcoronatracker.com/static/JSON/vimin.json', '#vimin_body');
+});
+
+function update(id, count) {
+    document.getElementById(id).innerHTML = (count.toLocaleString());
+}
+
+function search(input_id, tbody_id) {
+    let input, filter, body, tr, td, i, txtValue;
+    input = document.getElementById(input_id);
+    filter = input.value.toUpperCase();
+    body = document.getElementById(tbody_id);
+    tr = body.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("th")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
