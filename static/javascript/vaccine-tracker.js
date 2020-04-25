38b96607c15860c61886539466790dfc47ff5f1d
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-25 17:41:59 +0800

    Update vaccine-tracker.js

--- static/javascript/vaccine-tracker.js
+++ static/javascript/vaccine-tracker.js
@@ -1,30 +1,70 @@
-$('#chartContainer').CanvasJSChart({
-    animationEnabled: true,
-    zoomEnabled: true,
-    backgroundColor: "#F5F5F5",
-    legend:{
-        cursor: "pointer",
-        itemclick: explodePie
-    },
-    data: [{
-        type: "pie",
-        showInLegend: true,
-        toolTipContent: "{name}: <strong>{y}</strong>",
-        indexLabel: "{name} - {y}",
-        dataPoints: [
-            { y: 92, name: "Vaccines", exploded: true },
-            { y: 45, name: "Antibodies" },
-            { y: 20, name: "Anti-virals" },
-            { y: 12, name: "Cell-Based Therapies" },
-            { y: 5, name: "RNA-Based" },
-            { y: 12, name: "For Validation" },
-            { y: 4, name: "Devices"},
-            { y: 2, name: "Discontinued"},
-            { y: 52, name: "Others"}
-        ]
-    }]
-});
+$('#top').hide();    
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() > 100) {
+            $('#top').fadeIn(75);
+        }
+        else {
+            $('#top').fadeOut();
+        }
+    });
+    $('#top').on('click', function() {
+        $('html, body').animate({scrollTop: 0}, "fast");
+    });
     
+    $('#chartContainer').CanvasJSChart({
+        animationEnabled: true,
+        zoomEnabled: true,
+        backgroundColor: "#F5F5F5",
+        legend:{
+            cursor: "pointer",
+            itemclick: explodePie
+        },
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "{name}: <strong>{y}</strong>",
+            indexLabel: "{name} - {y}",
+            dataPoints: [
+                { y: 28, name: "Protein Sub-unit", exploded: true },
+                { y: 8, name: "DNA-Based" },
+                { y: 14, name: "RNA-Based" },
+                { y: 18, name: "Non-Replicating Viral Vector" },
+                { y: 4, name: "Virus-like Particle" },
+                { y: 2, name: "Replicating Viral Vector" },
+                { y: 5, name: "Inactive Viruses"},
+                { y: 14, name: "Unknown"}
+            ]
+        }]
+    });
+    
+    $('#treatment_container').CanvasJSChart({
+        animationEnabled: true,
+        zoomEnabled: true,
+        backgroundColor: "#F5F5F5",
+        legend:{
+            cursor: "pointer",
+            itemclick: explodePie
+        },
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "{name}: <strong>{y}</strong>",
+            indexLabel: "{name} - {y}",
+            dataPoints: [
+                { y: 49, name: "Antibodies", exploded: true },
+                { y: 20, name: "Antivirals" },
+                { y: 12, name: "Cell-Based Therapies" },
+                { y: 5, name: "RNA-Based" },
+                { y: 2, name: "Discontinued" },
+                { y: 4, name: "Devices" },
+                { y: 53, name: "Others"},
+                { y: 14, name: "For Validation"}
+            ]
+        }]
+    });
+});
+
 function explodePie (e) {
     if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
         e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
