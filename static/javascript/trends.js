504593370ac9f61f691d6359b506de6e34e6d906
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-28 00:21:47 +0800

    Update trends.js

--- static/javascript/trends.js
+++ static/javascript/trends.js
@@ -6,26 +6,33 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['Jan 30', 'Feb 5', 'Mar 15', 'Mar 29', 'Apr 12', 'Apr 25', 'Apr 26', 'Apr 27'],
+        labels: ['Jan 30', 'Feb 2', 'Feb 5', 'Feb 8', 'Feb 11', 'Feb 14', 'Feb 17', 'Feb 20', 'Feb 23', 'Feb 26', 'Feb 29',
+        'Mar 3', 'Mar 6', 'Mar 9', 'Mar 12', 'Mar 15', 'Mar 18', 'Mar 21', 'Mar 24', 'Mar 27', 'Mar 30',
+        'Apr 2', 'Apr 5', 'Apr 8', 'Apr 11', 'Apr 14', 'Apr 17', 'Apr 20', 'Apr 23', 'Apr 26'],
         datasets: [{
             label: 'Total Fatalities',
             backgroundColor: 'rgba(255, 255, 255, 0)',
             borderColor: '#e53e3e',
             pointBackgroundColor: '#e53e3e',
-            pointRadius: 4,
+            pointRadius: 0,
             hitRadius: 10,
             hoverRadius: 5,
-            data: [0, 1, 8, 71, 297, 494, 501, 511],
+            data: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+            1, 1, 1, 2, 11, 17, 19, 35, 54, 78,
+            107, 152, 182, 247, 335, 287, 428, 462, 501],
             lineTension: 0
         },{
             label: 'Total Recoveries',
             backgroundColor: 'rgba(255, 255, 255, 0)',
             borderColor: '#2BA366',
             pointBackgroundColor: '#2BA366',
-            pointRadius: 4,
+            pointRadius: 0,
             hitRadius: 10,
             hoverRadius: 5,
-            data: [0, 1, 2, 42, 197, 792, 862, 932],
+            data: [
+                0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 
+                1, 1, 1, 2, 2, 5, 13, 20, 31, 42,
+                51, 64, 96, 157, 295, 487, 613, 722, 862],
             lineTension: 0
         }]
     },
@@ -43,7 +50,7 @@ var chart = new Chart(ctx, {
             yAxes: [{
                 type: 'logarithmic',
                 ticks: {
-                    min: 1,
+                    min: 0,
                     max: 1000,
                     fontColor: '#333333',
                     callback: function (value, index, values) {
@@ -52,6 +59,7 @@ var chart = new Chart(ctx, {
                 },
                 afterBuildTicks: function (chartObj) { //Build ticks labelling as per your need
                     chartObj.ticks = [];
+                    chartObj.ticks.push(0);
                     chartObj.ticks.push(1);
                     chartObj.ticks.push(10);
                     chartObj.ticks.push(100);
@@ -64,10 +72,8 @@ var chart = new Chart(ctx, {
             }],
             xAxes: [{
                 ticks: {
-                    autoSkip: false,
-                    maxRotation: 145,
-                    minRotation: 35,
-                    fontColor: '#333333'
+                    fontColor: '#333333',
+                    maxTicksLimit: 8.1
                 },
                 gridLines: {
                     display: true,
@@ -84,16 +90,20 @@ var chart_1 = new Chart(ctx_1, {
 
     // The data for our dataset
     data: {
-        labels: ['Jan 30', 'Feb 5', 'Mar 15', 'Mar 29', 'Apr 12', 'Apr 25', 'Apr 26', 'Apr 27'],
+        labels: ['Jan 30', 'Feb 2', 'Feb 5', 'Feb 8', 'Feb 11', 'Feb 14', 'Feb 17', 'Feb 20', 'Feb 23', 'Feb 26', 'Feb 29',
+        'Mar 3', 'Mar 6', 'Mar 9', 'Mar 12', 'Mar 15', 'Mar 18', 'Mar 21', 'Mar 24', 'Mar 27', 'Mar 30',
+        'Apr 2', 'Apr 5', 'Apr 8', 'Apr 11', 'Apr 14', 'Apr 17', 'Apr 20', 'Apr 23', 'Apr 26'],
         datasets: [{
             label: 'Total Confirmed Cases',
             backgroundColor: 'rgba(255, 255, 255, 0)',
             borderColor: '#236377',
             pointBackgroundColor: '#236377FF',
-            pointRadius: 4,
+            pointRadius: 0,
             hitRadius: 10,
             hoverRadius: 5,
-            data: [1, 3, 140, 1418, 4648, 7294, 7579, 7777],
+            data: [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
+            3, 5, 20, 52, 140, 202, 307, 552, 803, 1546,
+            2633, 3246, 3870, 4428, 5223, 5878, 6459, 6981, 7579],
             lineTension: 0
         }]
     },
@@ -133,10 +143,8 @@ var chart_1 = new Chart(ctx_1, {
             }],
             xAxes: [{
                 ticks: {
-                    autoSkip: false,
-                    maxRotation: 145,
-                    minRotation: 35,
-                    fontColor: '#333333'
+                    fontColor: '#333333',
+                    maxTicksLimit: 8.1
                 },
                 gridLines: {
                     display: true,
@@ -144,5 +152,6 @@ var chart_1 = new Chart(ctx_1, {
                 }
             }]
         }
+        
     }
 });
