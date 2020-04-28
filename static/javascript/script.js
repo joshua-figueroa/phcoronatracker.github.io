ad5296b812d8b3b0717188886875367a78f9cca8
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-04-28 22:48:28 +0800

    Update script.js

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
         dataType: 'json',
         success: function(data) {
             for(var i = 0; i < data.length; i++) {
-                var row = $('<tr><th scope="row">' + data[i].name + '</th><td>' + data[i].cases + '</td><td>' + data[i].death +'</td><td>' + data[i].rec + '</td><td>' + data[i].active + '</td></tr>');
+                var row = $('<tr><th scope="row">' + data[i].name + '</th><td>' + data[i].cases.toLocaleString() + '</td><td>' + data[i].death.toLocaleString() +'</td><td>' + data[i].rec.toLocaleString() + '</td><td>' + data[i].active.toLocaleString() + '</td></tr>');
                 $('#ofw_body').append(row);
             }
         },
