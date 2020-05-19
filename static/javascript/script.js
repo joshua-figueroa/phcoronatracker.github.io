fad487c1c4204a4fb77aaade7045889e0c050149
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-05-19 22:39:30 +0800

    Update script.js

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -13,6 +13,26 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: 0}, "fast");
     });
 
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $dropdown.hover(
+        function() {
+            const $this = $(this);
+            $this.addClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "true");
+            $this.find($dropdownMenu).addClass(showClass);
+        },
+        function() {
+            const $this = $(this);
+            $this.removeClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "false");
+            $this.find($dropdownMenu).removeClass(showClass);
+        }
+    );
+
     $.ajax({
         url: 'https://phcoronatracker.com/static/JSON/cases.json',
         dataType: 'json',
