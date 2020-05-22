5e90b54a3b936164611e313c7f4f44cd3e2d94a0
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-05-23 01:43:26 +0800

    Update database.js

--- static/javascript/database.js
+++ static/javascript/database.js
@@ -30,7 +30,86 @@ $(document).ready(function() {
             $this.find($dropdownMenu).removeClass(showClass);
         }
     );
+
+    var ctx1 = document.getElementById('sex')
+    var ctx2 = document.getElementById('active-cases')
+    var chart1 = new Chart(ctx1, {
+        // The type of chart we want to create
+        type: 'pie',
     
+        // The data for our dataset
+        data: {
+            labels: ['Male', 'Female'],
+            datasets: [{
+                backgroundColor: ["#6CA0DC","#FF6961"],
+                data: [7397, 6200]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            tooltips: {
+                callbacks: {
+                    label: function (tooltipItem, data) {
+                        try {
+                            let label = ' ' + data.labels[tooltipItem.index]
+                            const value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+                            const sum = data.datasets[0].data.reduce((accumulator, curValue) => {
+                                return accumulator + curValue;
+                            });
+                            
+                            label += ': ' + value + ' (' + Number((value / sum) * 100).toFixed(2) + '%)'
+                            
+                            return label;
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    }
+                }
+            }
+        }
+    });
+    var chart2 = new Chart(ctx2, {
+        // The type of chart we want to create
+        type: 'pie',
+    
+        // The data for our dataset
+        data: {
+            labels: ['Asymtomatic', 'Mild', 'Severe', 'Critical'],
+            datasets: [{
+                backgroundColor: ["#6CA0DC","#FFC154","#FF6961","#BE61CA"],
+                data: [842, 8730, 54, 22]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            tooltips: {
+                callbacks: {
+                    label: function (tooltipItem, data) {
+                        try {
+                            let label = ' ' + data.labels[tooltipItem.index]
+                            const value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+                            const sum = data.datasets[0].data.reduce((accumulator, curValue) => {
+                                return accumulator + curValue;
+                            });
+
+                            label += ': ' + value + ' (' + Number((value / sum) * 100).toFixed(2) + '%)'
+                            
+                            return label;
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    }
+                }
+            }
+        }
+    });
+
     $.fn.DataTable.ext.pager.numbers_length = 6;
     $('#example').DataTable({
         "lengthMenu": [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"]],
