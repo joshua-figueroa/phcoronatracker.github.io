728faf0241760c4e0035f117a3c1f63710ed55ca
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-05-24 23:52:53 +0800

    Update timeline.js

--- static/javascript/timeline.js
+++ static/javascript/timeline.js
@@ -1,7 +1,20 @@
+$('#top').hide();
 $(document).ready(function() {
     document.getElementById("filter").value = "may";
     document.getElementById("filter-sm").value = "may";
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() > 100) {
+            $('#top').fadeIn(75);
+        }
+        else {
+            $('#top').fadeOut();
+        }
+    });
+    $('#top').on('click', function() {
+        $('html, body').animate({scrollTop: 0}, "fast");
+    });
 });
+
 var prev = "may";
 function display(data) {
     document.getElementById(prev).style.display = "none";
