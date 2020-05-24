5bbed3a6a9b05dd0138a2fa5c5e31217506a00fd
phcoronatracker <joshuavillartafigueroa@gmail.com>
2020-05-24 15:54:48 +0800

    Update timeline.js

--- static/javascript/timeline.js
+++ static/javascript/timeline.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
 var prev = "jan";
 function display(data) {
     document.getElementById(prev).style.display = "none";
-    console.log(prev)
     document.getElementById(data).style.display = "block";
     prev = data;
-    console.log(prev)
 }
